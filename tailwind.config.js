b2e96ab90e805a48815863f3e9c7a35a2edb0c8c
talles <talles17.a@hotmail.com>
2023-02-26 13:48:05 -0300

    fix: set inter fontFamily

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,17 @@
 /** @type {import('tailwindcss').Config} */
+
+
 module.exports = {
   content: [
     "./src/pages/**/*.{js,ts,jsx,tsx}",
     "./src/components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ['Inter var', 'sans'],
+      },
+    },
   },
   plugins: [],
 }
