9b4e6711fb431cbdde9d3a18b92ec43ea43fc4c8
talles <talles17.a@hotmail.com>
2023-03-14 15:05:14 -0300

    feat: add Checkbox component

---
+++ src/components/Form/Checkbox.tsx
@@ -0,0 +1,34 @@
+import { ForwardedRef, forwardRef } from 'react';
+
+interface CheckboxProps {
+  id: string;
+  label: string;
+}
+
+const Checkbox = forwardRef(
+  (
+    { id, label, ...rest }: CheckboxProps,
+    ref: ForwardedRef<HTMLInputElement>,
+  ) => {
+    return (
+      <div className="inline-flex items-center space-x-1.5">
+        <input
+          id={id}
+          type="checkbox"
+          value={id}
+          ref={ref}
+          className="cursor-pointer rounded border-gray-300 text-blue-600 transition focus:ring-blue-600 disabled:cursor-not-allowed disabled:bg-gray-200 disabled:opacity-75"
+          {...rest}
+        />
+        <label
+          htmlFor="private"
+          className="truncate text-sm font-medium text-gray-500"
+        >
+          {label}
+        </label>
+      </div>
+    );
+  },
+);
+
+export default Checkbox;
