93e68cc2a7d202b2cb39ce37edab5e474b5cfbfb
talles <cleydson.vieira@ab-inbev.com>
2023-03-27 23:51:39 -0300

    feat: add redirection auth funcitons

---
+++ src/utils/withSSRAuth.ts
@@ -0,0 +1,25 @@
+import {
+  GetServerSideProps,
+  GetServerSidePropsContext,
+  GetServerSidePropsResult,
+} from 'next';
+import { parseCookies } from 'nookies';
+
+export default function withSSRAuth<P>(fn: GetServerSideProps<P>) {
+  return async (
+    ctx: GetServerSidePropsContext,
+  ): Promise<GetServerSidePropsResult<P>> => {
+    const cookies = parseCookies(ctx);
+    const token = cookies['teethaligner.token'];
+
+    if (!token) {
+      return {
+        redirect: {
+          destination: '/login',
+          permanent: false,
+        },
+      };
+    }
+    return fn(ctx);
+  };
+}
