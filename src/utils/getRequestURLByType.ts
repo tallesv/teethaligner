6ff92ec0abc8cf0bea1347af2bae29c327597420
Talles <talles17.a@hotmail.com>
2024-01-04 15:34:20 -0300

    feat: function to generate request url

---
+++ src/utils/getRequestURLByType.ts
@@ -0,0 +1,16 @@
+export default function getRequestURLByType(productName: string) {
+  switch (productName) {
+    case 'Setup':
+      return `/products/setup`;
+    case 'Alinhadores - Programação TeethAligner':
+      return `/products/alinhadores/programacao-teethaligner`;
+    case 'Alinhadores - Apenas Imprimir':
+      return `/products/alinhadores/apenas-imprimir`;
+    case 'Moldagem de Transferência Virtual':
+      return `/products/transferencia-virtual`;
+    case 'Modelos/Guias Cirúrgicos':
+      return `/products/guias-cirurgicos`;
+    default:
+      return '';
+  }
+}
